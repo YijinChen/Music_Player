2168d7cfc8fc03f211d16daf8e2b9d8ddaf0dbdb
“YijinChen” <452335450@qq.com>
2024-07-11 16:08:11 +0200

    add socket code and music files

---
+++ socket.c
@@ -0,0 +1,48 @@
+//network relative code
+#include <stdio.h>
+#include <sys/socket.h>
+#include "main.h"
+#include <pthread.h>
+#include "socket.h"
+#include <string.h>
+#include "device.h"
+extern int g_sockfd;
+
+void *connect_cb(void *arg){
+    int count = 5;
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = PF_INET;
+    server_addr.sin_port = SERVER_PORT;
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+    while(count--){
+        //when sending connection request to server, let the first led shine
+        led_on(0);
+        int ret = connect(g_sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr));
+        if (ret == -1){
+            continue;
+        }
+        //when connection is successful, let all 4 leds shine
+        led_on(0);
+        led_on(1);
+        led_on(2);
+        led_on(3);
+
+        break;
+    }
+}
+
+int InitSocket(){
+    g_sockfd = socket(PF_INET, SOCK_STREAM, 0);
+    if (g_sockfd == -1){
+        return FAILURE;
+    }
+
+    //creat a thread and request the connection to server
+    pthread_t tid;
+    int ret = pthread_create(&tid, NULL, connect_cb, NULL);
+    if (ret != 0){
+        return FAILURE;
+    }
+
+}
---
+++ socket.h
@@ -0,0 +1,7 @@
+#ifndef SOCKET_H
+#define SOCKET_H
+
+#define SERVER_PORT 8000
+#define DERVER_IP "18.185.92.160"//the public address of server
+
+#endif
