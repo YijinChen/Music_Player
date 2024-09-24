8b5fe7afaee9187cdd8c08e1c86c38b04e93a850
“YijinChen” <452335450@qq.com>
2024-09-24 18:38:51 +0200

    finished the driver for extended buttons

--- music_player/button_driver/button_test.c
+++ music_player/button_driver/button_test.c
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
+#include <poll.h>
 
 /*
  * ./button_test /dev/100ask_button0
@@ -13,7 +14,10 @@
 int main(int argc, char **argv)
 {
 	int fd;
-	char val;
+	int val;
+	struct pollfd fds[1];
+	int timeout_ms = 5000;
+	int ret;
 	
 	/* 1. 判断参数 */
 	if (argc != 2) 
@@ -30,9 +34,24 @@ int main(int argc, char **argv)
 		return -1;
 	}
 
-	/* 3. 写文件 */
-	read(fd, &val, 1);
-	printf("get button : %d\n", val);
+	fds[0].fd = fd;
+	fds[0].events = POLLIN;
+	
+
+	while (1)
+	{
+		/* 3. 读文件 */
+		ret = poll(fds, 1, timeout_ms);
+		if ((ret == 1) && (fds[0].revents & POLLIN))
+		{
+			read(fd, &val, 4);
+			printf("get button : 0x%x\n", val);
+		}
+		else
+		{
+			printf("timeout\n");
+		}
+	}
 	
 	close(fd);
 	
